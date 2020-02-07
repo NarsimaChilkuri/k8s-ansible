adad10a706a54a7a75d00f0c1cb4bc2d5e240778
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2020-02-07 19:17:27 +0530

    validation of past installation package

--- setup-k8s-cluster.sh
+++ setup-k8s-cluster.sh
@@ -3,32 +3,70 @@
 
   if [ -f /etc/redhat-release ]; then
     yum update
-    echo "--------- INSTALLING VIRTUALBOX ----------"
-    sudo yum install –y patch gcc kernel-headers kernel-devel make perl wget
-    sudo wget http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -P /etc/yum.repos.d
-    sudo yum install VirtualBox-6.0
-    echo "--------- INSTALLING VAGRANT ----------"
-    sudo yum install https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.rpm
-    vagrant --version
-    echo "--------- INSTALLING ANSIBLE ----------"
-    sudo yum install ansible
+
+    which virtualbox
+    if [ $? = '0' ]; then
+      echo "VIRTUALBOX ALREADY INSTALLED"
+    else
+      echo "--------- INSTALLING VIRTUALBOX ----------"
+      sudo yum install –y patch gcc kernel-headers kernel-devel make perl wget
+      sudo wget http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -P /etc/yum.repos.d
+      sudo yum install VirtualBox-6.0
+    fi
+
+    which vagrant
+    if [ $? = '0' ]; then
+      echo "VAGRANT ALREADY INSTALLED"
+    else
+      echo "--------- INSTALLING VAGRANT ----------"
+      sudo yum install https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.rpm
+      vagrant --version
+    fi
+
+    which ansible
+    if [ $? = '0' ]; then
+      echo "ANSIBLE ALREADY INSTALLED"
+    else
+      echo "--------- INSTALLING ANSIBLE ----------"
+      sudo yum install ansible
+    fi
     vagrant up
     vagrant ssh k8s-master
+
   elif [ -f /etc/lsb-release ]; then
     apt-get update
-    echo "--------- INSTALLING VIRTUALBOX ----------"
-    sudo apt install virtualbox
-    echo "--------- INSTALLING VAGRANT ----------"
-    curl -O https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb
-    sudo apt install ./vagrant_2.2.6_x86_64.deb
-    vagrant --version
-    sudo apt update
-    echo "--------- INSTALLING ANSIBLE ----------"
-    sudo apt install software-properties-common
-    sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo apt install ansible
+
+    which virtualbox
+    if [ $? = '0' ]; then
+      echo "VIRTUALBOX ALREADY INSTALLED"
+    else  
+      echo "--------- INSTALLING VIRTUALBOX ----------"
+      sudo apt install virtualbox
+    fi
+    
+    which vagrant
+    if [ $? = '0' ]; then
+      echo "VAGRANT ALREADY INSTALLED"
+    else
+      echo "--------- INSTALLING VAGRANT ----------"
+      curl -O https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb
+      sudo apt install ./vagrant_2.2.6_x86_64.deb
+      vagrant --version
+    fi
+
+    which ansible
+    if [ $? = '0' ]; then
+      echo "ANSIBLE ALREADY INSTALLED"
+    else
+      sudo apt update
+      echo "--------- INSTALLING ANSIBLE ----------"
+      sudo apt install software-properties-common
+      sudo apt-add-repository --yes --update ppa:ansible/ansible
+      sudo apt install ansible
+    fi
     vagrant up
     vagrant ssh k8s-master
+
   else  
     echo 'WARN: Could not detect distro or distro unsupported'
   fi
